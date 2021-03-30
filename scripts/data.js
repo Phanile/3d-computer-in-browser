be1cf0f013c88337ae9fa169a8852df3b6db0a5b
Phanile <56351259+Phanile@users.noreply.github.com>
2021-03-30 18:47:23 +0300

    add gui + add new pc obj + add buttons

---
+++ scripts/data.js
@@ -0,0 +1,41 @@
+const computers = [
+    {
+        'id' : 1,
+        'name' : 'Gaming Wind 5',
+        'price' : 1500,
+        'currency' : 'usd',
+        'pathToModel' : '',
+        'specifications' : {
+            'videocard' : 'gtx 2060',
+            'proccesor' : 'ryzen 5 2600',
+            'ram' : 16,
+            'data' : 1000
+        }
+    },
+    {
+        'id' : 2,
+        'name' : 'Gaming Wind 7',
+        'price' : 2000,
+        'currency' : 'usd',
+        'pathToModel' : 'objects/case2.obj',
+        'specifications' : {
+            'videocard' : 'gtx 2070',
+            'proccesor' : 'ryzen 5 3600',
+            'ram' : 16,
+            'data' : 1000
+        }
+    },
+    {
+        'id' : 3,
+        'name' : 'Smart NC301',
+        'price' : 675,
+        'currency' : 'usd',
+        'pathToModel' : '',
+        'specifications' : {
+            'videocard' : 'gtx 1050',
+            'proccesor' : 'ryzen 5 2600',
+            'ram' : 16,
+            'data' : 1000
+        }
+    },
+]

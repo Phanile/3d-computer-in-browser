19d5de2d412b6622dab82c3ed5060c9b2d1d55fe
Phanile <56351259+Phanile@users.noreply.github.com>
2021-04-04 08:09:46 +0300

    add camera movement

--- scripts/data.js
+++ scripts/data.js
@@ -10,32 +10,35 @@ const computers = [
             'proccesor' : 'ryzen 5 2600',
             'ram' : 16,
             'data' : 1000
-        }
+        },
+        'load' : false
     },
     {
         'id' : 2,
         'name' : 'Gaming Wind 7',
         'price' : 2000,
         'currency' : 'usd',
-        'pathToModel' : 'objects/case2.obj',
+        'pathToModel' : 'objects/Case2/case2.obj',
         'specifications' : {
             'videocard' : 'gtx 2070',
             'proccesor' : 'ryzen 5 3600',
             'ram' : 16,
             'data' : 1000
-        }
+        },
+        'load' : true,
     },
     {
         'id' : 3,
         'name' : 'Smart NC301',
         'price' : 675,
         'currency' : 'usd',
-        'pathToModel' : '',
+        'pathToModel' : 'objects/Case2/case2.obj',
         'specifications' : {
             'videocard' : 'gtx 1050',
             'proccesor' : 'ryzen 5 2600',
             'ram' : 16,
             'data' : 1000
-        }
+        },
+        'load' : true,
     },
 ]
